355486f451f23ad999eea6c5ead5e6e94ec6f012
Osuminer <connorstaschiak@gmail.com>
2022-09-12 19:22:28 -0400

    ArrayBasedStack start

--- Lab02/StackQueue.cpp
+++ Lab02/StackQueue.cpp
@@ -18,10 +18,61 @@ int ArrayBasedStack::peek() const
 
 }
 
+ArrayBasedStack::ArrayBasedStack() {
+	for (int i = 0; i < 4; i++) {
+		tower[i] = 0;
+	}
+}
+
+bool ArrayBasedStack::isEmpty() const{
+	for (int  i = 0; i < 4; i++) {
+		if (tower[i] != 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
 bool ArrayBasedStack::push(const int & val) {
+
+	for (int i = 0; i < 4; i++) {
+		if (tower[i] == 0) {
+			tower[i] = val;
+
+			return true;
+		}
+
+		i++;
+	}
+
+	return false;
+}
+
+bool ArrayBasedStack::pop() {
+
+	for (int i = 3; i >= 0; i--) {
+		if (tower[i] != 0) {
+			tower[i] = 0;
+
+			return true;
+		}
+
+		i--;
+	}
+
+	
+
+	return false;
+}
+
+std::string ArrayBasedStack::toString() const{
 
 }
 
+ArrayBasedStack::~ArrayBasedStack() {
+	
+}
 
 
 std::string ArrayBasedQueue::peekFront() const
