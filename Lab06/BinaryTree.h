95c7b76781098de845c03dd84c18a4a687164085
Osuminer <connorstaschiak@gmail.com>
2022-10-24 18:57:45 -0400

    Files added and classes declared

---
+++ Lab06/BinaryTree.h
@@ -0,0 +1,40 @@
+template<typename T> class BinaryTree {
+    public:
+        BinaryTree() {
+
+        }
+
+        ~BinaryTree() {
+
+        }
+
+        bool Insert(Node node) {
+
+        }
+
+        Node* Find() {
+
+        }
+
+        int Size() {
+
+        }
+
+        std::vector<Node> GetAllAscending() {
+
+        }
+
+        void Clear() {
+
+        }
+
+    private:
+
+};
+
+template<typename T> class Node {
+    public:
+
+    private:
+
+};
