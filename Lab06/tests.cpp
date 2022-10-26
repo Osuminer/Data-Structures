b553c9d7ceae8fc4e8b1b5d4807ab94e5a3c5287
strifeja <strifeja@mail.uc.edu>
2022-10-26 19:13:04 -0400

    test stuffs

--- Lab06/tests.cpp
+++ Lab06/tests.cpp
@@ -4,11 +4,29 @@
 // Insert test cases ****************************************
 
 TEST(Insert, insert1){
+    BinaryTree<int> cheese;
+    Node<int> one(1);
+    Node<int> two(2);
+    cheese.Insert(one); 
+    ASSERT_TRUE( cheese.Insert(two) );
 
 }
 
 TEST(Insert, insert2){
+    BinaryTree<int> cheese;
+    Node<int> one(1);
+    Node<int> two(2);
+    Node<int> three(3);
+    Node<int> four(4);
+    Node<int> five(5);
 
+    cheese.Insert(one);
+    cheese.Insert(two);
+    cheese.Insert(three);
+    cheese.Insert(four);
+    cheese.Insert(five);
+
+    ASSERT_EQ( cheese.Size() , 5);
 }
 
 // Find test cases ******************************************
@@ -48,5 +66,5 @@ TEST(Clear, clear1){
 }
 
 TEST(Clear, clear2){
-    
+
 }
