870d8e4311f697b7b46dd520bbec8a5536a5d05e
Osuminer <connorstaschiak@gmail.com>
2022-10-26 12:44:40 -0400

    updates made

--- Lab06/BinaryTree.h
+++ Lab06/BinaryTree.h
@@ -1,26 +1,32 @@
 template<typename T> class BinaryTree {
     public:
         BinaryTree() {
-
+            m_size = 0;
         }
 
         ~BinaryTree() {
 
         }
 
-        bool Insert(Node node) {
+        bool Insert(Node<T>* node) {
+            if (m_size == 0) {
+                
+                return true;
+            }
+
+            if (root.)
 
         }
 
-        Node* Find() {
+        Node<T>* Find(T val) {
 
         }
 
         int Size() {
-
+            return m_size;
         }
 
-        std::vector<Node> GetAllAscending() {
+        std::vector<Node<T>> GetAllAscending() {
 
         }
 
@@ -29,12 +35,53 @@ template<typename T> class BinaryTree {
         }
 
     private:
-
+        int m_size;
 };
 
 template<typename T> class Node {
     public:
+        Node() {
+            m_val = NULL;
+            m_right = nullptr;
+            m_left = nullptr;
+        }
+
+        Node(Node* node) {
+            this->m_left = node->GetLeftNode();
+            this->m_right = node->GetRightNode();
+            this->m_val = node->ReturnValue();
+        }
+
+        Node(T val) {
+            m_val = val;
+        }
+
+        ~Node() {
+
+        }
+
+        T ReturnValue() {
+            return m_val;
+        }
+
+        Node* GetRightNode() {
+            return m_right;
+        }
+
+        void SetRightNode(Node* n) {
+            m_right = *n;
+        }
+
+        Node* GetLeftNode() {
+            return m_left;
+        }
+
+        void SetLeftNode(Node* n) {
+            m_left = *n;
+        }
 
     private:
-
+        T m_val;
+        Node* m_right = nullptr;
+        Node* m_left = nullptr;
 };
--- Lab06/main.cpp
+++ Lab06/main.cpp
@@ -1,2 +1,11 @@
 #include "BinaryTree.h"
 
+int main() {
+
+    Node<int> node(10, nullptr, nullptr);
+
+    BinaryTree<int> BST();
+
+
+    return 0;
+}
