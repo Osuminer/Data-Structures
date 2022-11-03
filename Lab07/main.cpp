0d766581fff41a0d226c0da5b1e80cd7cc311f93
strifeja <strifeja@mail.uc.edu>
2022-11-02 20:37:28 -0400

    main stuf

--- Lab07/main.cpp
+++ Lab07/main.cpp
@@ -3,7 +3,43 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <chrono>
+
+typedef chrono::high_resolution_clock Clock;
 
 using namespace std;
 
-int main(){}
+int main(){
+
+    srand(1);   // Seed array to 1
+
+    const int ARRAYSIZE = 500;
+    //const int ARRAYSIZE = 1000; 
+    //const int ARRAYSIZE = 2000; 
+    //const int ARRAYSIZE = 5000; 
+    
+      // Size of array
+    int arr[ARRAYSIZE];         // Array to sort
+
+    // Fill array with random numbers from 0 to RANDMAX
+    for (int i = 0; i < ARRAYSIZE; i++) {
+        arr[i] = rand();
+    }
+
+    auto t1 = Clock::now();
+
+    ArrayBasedPriorityQueue alpha;
+    //HeapBasedPriorityQueue alpha;
+
+    for (int i = 0; i < ARRAYSIZE; i++) {
+        alpha.Insert(arr[i]);
+        //alpha.Dequeue(arr[i]);
+    }
+    
+    auto t2 = Clock::now();
+
+    cout << "\nTime taken: " << chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count() << "ns\n" << endl;
+
+    return 0;
+
+}
