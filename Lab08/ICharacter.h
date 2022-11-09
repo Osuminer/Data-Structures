b3744bec72b8eafc3c7e36fcabf6e50546bd14e0
Osuminer <connorstaschiak@gmail.com>
2022-11-09 17:09:56 -0500

    IRace and ICharacter

--- Lab08/ICharacter.h
+++ Lab08/ICharacter.h
@@ -1,5 +1,19 @@
-class Class {
+#pragma once
+
+#include "IRace.h"
+#include <memory>
+
+class ICharacter {
     public:
+        ICharacter(std::shared_ptr<IRace> race) {
+            m_health = 0;
+            m_armor = 0;
+            m_damage = 0;
+            m_speed = 0;
+            m_crit = 0;
+            m_race = race;
+        }
+
         double GetHealth(){return m_health;}
         double GetArmor(){return m_armor;}
         double GetDamage(){return m_damage;}
@@ -7,6 +21,7 @@ class Class {
         double GetCrit(){return m_crit;}
 
     protected:
+        std::shared_ptr<IRace> m_race;
         double m_health;
         double m_armor;
         double m_damage;
--- Lab08/IRace.h
+++ Lab08/IRace.h
@@ -1,5 +1,17 @@
+#pragma once
+
 class IRace {
     public:
+        IRace(double health_mod, double armor_mod, double speed_mod, double damage_mod, double crit_mod) {
+            m_health_mod = health_mod;
+            m_armor_mod = armor_mod;
+            m_speed_mod = speed_mod;
+            m_damage_mod = damage_mod;
+            m_crit_mod = crit_mod;
+        }
+
+        ~IRace() {}
+
         double GetHealthMod(){return m_health_mod;}
         double GetArmorMod(){return m_armor_mod;}
         double GetSpeedMod(){return m_speed_mod;}
