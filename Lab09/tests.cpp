63beab609e7d4858a54ac3245da38a816576ca33
Osuminer <connorstaschiak@gmail.com>
2022-11-20 16:15:37 -0500

    Main. test and cmake

---
+++ Lab09/tests.cpp
@@ -0,0 +1,64 @@
+#include <gtest/gtest.h>
+#include "Graph.h"
+
+
+// Add Edges ----------------------------------------------------------------------
+TEST(AddEdges, AddEdge1) {
+
+}
+
+TEST(AddEdges, AddEdge2) {
+    
+}
+
+// Remove Edges -------------------------------------------------------------------
+TEST(RemoveEdges, RemoveEdge1) {
+
+}
+
+TEST(RemoveEdges, RemoveEdge2) {
+    
+}
+
+// Has Edges ----------------------------------------------------------------------
+TEST(HasEdges, HasEdge1) {
+
+}
+
+TEST(HasEdges, HasEdge2) {
+    
+}
+
+// Out Edges ----------------------------------------------------------------------
+TEST(OutEdges, OutEdge1) {
+
+}
+
+TEST(OutEdges, OutEdge2) {
+    
+}
+
+// In Edges -----------------------------------------------------------------------
+TEST(InEdges, InEdge1) {
+
+}
+
+TEST(InEdges, InEdge2) {
+    
+}
+
+// Print --------------------------------------------------------------------------
+TEST(PrintMatrix, Print1) {
+
+}
+
+TEST(PrintMatrix, Print2) {
+    
+}
+
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
