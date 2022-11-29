b2a58572ec5fb93d6fdbef9934b6ab55b5b8d249
Osuminer <connorstaschiak@gmail.com>
2022-11-28 20:21:17 -0500

    main updated

--- Lab10/main.cpp
+++ Lab10/main.cpp
@@ -1,16 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
 #include "HashTable.h"
 #include "Node.h"
 
-int main() {
-    HashTable<int, Node<int>> h(5);
+typedef std::chrono::high_resolution_clock Clock;
 
-    Node<int> a(1, 7);
-    Node<int> b(7, 3);
-    Node<int> c(1, 4);
+using namespace std;
 
-    h.AddItem(a);
-    h.AddItem(b);
-    h.AddItem(c);
+int main(){
 
-    h.GetItem(c);
+    srand(1);   // Seed array to 1
+
+    HashTable<int, Node<int>> h1(500);
+    HashTable<int, Node<int>> h2(1000);
+    HashTable<int, Node<int>> h3(2000);
+    HashTable<int, Node<int>> h4(5000);
+
+    // Fill 500 item has table
+    for (int i = 0; i < 500; i++) {
+        Node<int> a(rand(), 2);
+
+        h1.AddItem(a);
+    }
+
+    // Fill 1000 item has table
+    for (int i = 0; i < 1000; i++) {
+        Node<int> a(rand(), 2);
+
+        h2.AddItem(a);
+    }
+
+    // Fill 2000 item has table
+    for (int i = 0; i < 2000; i++) {
+        Node<int> a(rand(), 2);
+
+        h3.AddItem(a);
+    }
+       
+    // Fill 5000 item has table
+    for (int i = 0; i < 5000; i++) {
+        Node<int> a(rand(), 2);
+
+        h4.AddItem(a);
+    }
+
+
+
+
+    // array insert ------------------------------------------------------------
+
+    auto arrayBeforeInsert = Clock::now(); 
+
+    
+    auto arrayAfterInsert = Clock::now(); 
+
+
+    cout << "\nTime taken array insert: " << chrono::duration_cast<chrono::nanoseconds>(arrayAfterInsert - arrayBeforeInsert).count() << "ns\n" << endl;
+
+
+    return 0;
+
 }
